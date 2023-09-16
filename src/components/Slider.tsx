c5d4a2694d14159345f667104c480b9731096d51
Tudor <tudor.comparan@gmail.com>
2023-09-16 14:22:20 -0600

    Slides 0 and 1

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 
@@ -17,10 +18,33 @@ export default function Slider({}) {
       modules={[Pagination, Navigation]}
       className="h-full "
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>
+        <Slide0 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Slide1 />
+      </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
     </Swiper>
+  );
+}
+
+function Slide0() {
+  return (
+    <div className="relative h-full">
+      <Image src="/images/intro.jpg" alt="slide zero" fill className="object-cover object-center" />;
+    </div>
+  );
+}
+function Slide1() {
+  return (
+    <div className="relative h-full">
+      <p className="absolute z-10 right-10 w-1/3 top-10 text-2xl">
+        Un joven discípulo solicitó al Maestro Iluminado el asistir en silencio a las entrevistas que éste concedía a aquellas personas que iban en busca de su consejo y sabiduría.
+      </p>
+      <Image src="/images/slide_1.jpg" alt="slide one" fill className="object-cover object-center" />;
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Slider from '@/components/Slider';
 import { Inter } from 'next/font/google';
 
@@ -7,7 +8,12 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
       <div className="w-full h-full bg-cyan-900 flex justify-center items-center p-4 text-white flex-col gap-2">
-        <h1 className="text-4xl">Un cuento interactivo 😋 </h1>
+        <Link
+          href="https://desarrolloweb.udgvirtual.udg.mx/mod/assign/view.php?id=5998"
+          className="text-4xl hover:text-blue-200 px-4 py-2 rounded-lg transition-all duration-300 hover:bg-white hover:bg-opacity-20"
+        >
+          Un cuento interactivo 😋
+        </Link>
         <p className="text-xl">Por Tudor Comparán</p>
       </div>
 
