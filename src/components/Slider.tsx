6c44f212c944c3b7783fab7ff8cc2e6075d4ef88
Tudor <tudor.comparan@gmail.com>
2023-09-16 11:55:55 -0600

    Slider an title

---
+++ src/components/Slider.tsx
@@ -0,0 +1,26 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+export default function Slider({}) {
+  return (
+    <Swiper
+      pagination={{ type: 'progressbar' }}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="h-full "
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+}
